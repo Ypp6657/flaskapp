f69e0019d3df58f3924b3172fbc0bb3c3fe683ec
Ypp6657 <yash-dev1@demo.lab>
2024-12-27 19:17:11 +0530

    TESTING

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return "Its Lunch Time...Modani ko bhook lagi hai !!!"
+    return "TESTINGGGGGGGGGGG !!!"
 
 if __name__ == "__main__" :
     app.run(host='0.0.0.0',debug=True)
